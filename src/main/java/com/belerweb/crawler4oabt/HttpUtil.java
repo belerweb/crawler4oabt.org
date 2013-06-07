4e49086458df519cec6c162b1e35bbdd1b76b4e8
belerweb <belerweb@gmail.com>
2013-06-07 17:25:07 +0800

    add HttpUtil & HttpUtil.get method declare

---
+++ src/main/java/com/belerweb/crawler4oabt/HttpUtil.java
@@ -0,0 +1,19 @@
+package com.belerweb.crawler4oabt;
+
+/**
+ * HTTP 工具类
+ */
+public class HttpUtil {
+
+  /**
+   * GET 请求，随机User-Agent，不保存COOKIE
+   * 
+   * @param url 请求网址
+   * @return 网页HTML代码
+   */
+  public static String get(String url) {
+    // TODO 实现
+    return null;
+  }
+
+}
